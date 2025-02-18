d9ae87bba0b36e83bd0fbb9c93ee5f9d089e50de
kejiwuxian <1012072118@qq.com>
2025-02-17 21:37:23 -0800

    Add dynamic length

--- Manipulating-Console-Output/main.cpp
+++ Manipulating-Console-Output/main.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-void print_row(int id, string name, double balance)
+void print_row(int id, string name, double balance, int name_length = 12)
 {
 	// Set right align, set the width of the id to 5 then set the fill character to '0' so it's zero padded.
 	cout << right << setw(5) << setfill('0') << id << "    "
 		// Reset the fill character to space, set left align then set the width of the Name column to 16 to align with the header.
-		<< setfill(' ')	<< left << setw(16) << name.substr(0,12)
+		<< setfill(' ')	<< left << setw(name_length + 4) << name.substr(0, name_length)
 		// Set the floatfield to fixed then set the precision of the Balance to 2 so it always has 2 decimal places.
 		<< "$" << fixed << setprecision(2) << balance << endl;
 }
@@ -25,24 +25,39 @@ int main()
 
 	string name1 = "Alice", name2 = "Bob", name3 = "Charlie";
 
-	double balance1 = 1000.0, balance2 = 530.25, balance3 = 2040.5;	
+	double balance1 = 1000.0, balance2 = 530.25, balance3 = 2040.5;
+
+	// The max name length.
+	int name_length = max(max(name1.length(), name2.length()), name3.length());
+
+	int max_balance = static_cast<int>(max(max(balance1, balance2), balance3));
+
+	// The max balance_length
+	int balance_length = 4;
+
+	do
+	{
+		balance_length++;
+		max_balance /= 10;
+	}
+	while (max_balance);
 
 	// Set the width of the title line to 67 so it's at the center of the my console window.
 	// Hope your console window size and font size are the same as mine.
 	cout << setw(67) << "Daily Report" << endl;
 
 	// Set left align then set the width of the ID column to 9 and the width of the Name column to 16 so they have some space after.
-	cout << left << setw(9) << "ID" << setw(16) << "Name" << "Balance" << endl;
+	cout << left << setw(9) << "ID" << setw(name_length + 4) << "Name" << "Balance" << endl;
 
 	// Left align is still in effect. Set the width of the ID and Name column to align with the headers.
 	cout << setw(5) << setfill('-') << "" << setw(4) << setfill(' ') << ""
-		<< setw(12) << setfill('-') << "" << setw(4) << setfill(' ') << "" 
-		<< setw(10) << setfill('-') << "" << endl;
+		<< setw(name_length) << setfill('-') << "" << setw(4) << setfill(' ') << ""
+		<< setw(balance_length) << setfill('-') << "" << endl;
 	//cout << setw(9) << "-----" << setw(16) << "------------" << "----------" << endl;
 
-	print_row(id1, name1, balance1);
-	print_row(id2, name2, balance2);
-	print_row(id3, name3, balance3);
+	print_row(id1, name1, balance1, name_length);
+	print_row(id2, name2, balance2, name_length);
+	print_row(id3, name3, balance3, name_length);
 
 	return 0;
 }
